5084320ed92ff61bf60b36065614c02a0e98acc4
ezzygarmyz <dragkhai@gmail.com>
2024-09-05 08:21:17 +0200

    update env_base.py

--- electrumz/lib/env_base.py
+++ electrumz/lib/env_base.py
@@ -1,9 +1,4 @@
-# Copyright (c) 2017, Neil Booth
-#
-# All rights reserved.
-#
-# See the file "LICENCE" for information about the copyright
-# and warranty status of this software.
+
 
 '''Class for server environment configuration and defaults.'''
 
